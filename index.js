5b6170f8cdf9d463438737f01f48d0956720e808
Ahamed Basith <141796686+adbasithsj@users.noreply.github.com>
2024-04-17 19:16:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let os=document.getElementById('os');
+function Clear(){
+    os.value="";
+}
+function del(){
+    os.value=os.value.slice(0,-1);
+}
+function display(txt){
+    os.value=os.value+txt;
+}
+function calculate(){
+
+    os.value=eval(os.value)
+
+}
